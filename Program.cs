9a306c504aac0d73e73b4702a43597c0f1cc14d7
Tyler Harris <tyhad04@student.oaisd.org>
2025-12-03 15:29:56 +0000

    Added character checker with a true or false statement to tell if the character was in the name or not

--- Program.cs
+++ Program.cs
@@ -3,6 +3,24 @@ public class Program
 {
     public static void Main(string []args)
     {
-        
+        Console.WriteLine("Enter a letter to search for:");
+        char characterToFind = Console.ReadLine()[0];
+    }
+    public static void characterChecker(string name, char characterToFind)
+    {
+        bool characterChecker = false;
+
+        foreach (char letter in name)
+        {
+            if (letter == characterToFind)
+            {
+                characterChecker = true;-
+            }
+            else
+            {
+                characterChecker = false;
+            }
+        }
+
     }
 }
