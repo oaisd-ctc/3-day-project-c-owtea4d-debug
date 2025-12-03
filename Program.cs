c6cc37350bc9a8968d411031da00add6435bc616
Izaac-Wilson-Student <izwi160@student.oaisd.org>
2025-12-03 15:00:40 +0000

    created array of names and a random number generator

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
 ﻿public class Program
 {
-
+    static string[] Names = {"martinez", "luyk", "benjamin", "caden", "carlos", "gabriel", "garnold", "izaac", "joseph", "judah", "keira", "lorilei", "lucas", "mason", "nuri","terhorst","walsh", "sage", "tyler"};
+    public static void Main(string[] args)
+    {
+        Random rnd = new Random();
+        int Namesi = rnd.Next(0, 18);
+        Console.WriteLine(Names[Namesi]);
+    }
 }
