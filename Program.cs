c3d3b352c7177fe6fc03106a459095bf55513ed1
OwenTerhorst <owtea4d@student.oaisd.org>
2025-12-03 14:41:06 +0000

    added a counter that adds 1 everytime the parameter bool equals false

--- Program.cs
+++ Program.cs
@@ -1,8 +1,36 @@
 ﻿using System;
 public class Program
 {
-    public static void Main(string []args)
+    public static void Main(string[] args)
     {
-        
+        theHangman(true);
+    }
+    public static void theHangman(bool n)
+    {
+        int counter = 0;
+        if (n == false)
+        {
+            counter = counter + 1;
+        }
+        if (counter == 0)
+        {
+            Console.WriteLine($"          +---+");
+            Console.WriteLine($"          |   |");
+            Console.WriteLine($"              |");
+            Console.WriteLine($"              |");
+            Console.WriteLine($"              |");
+            Console.WriteLine($"              |");
+            Console.WriteLine($"          =========");
+        }
+        else if(counter == 1)
+        {
+            Console.WriteLine($"          +---+");
+            Console.WriteLine($"          |   |");
+            Console.WriteLine($"          0   |");
+            Console.WriteLine($"              |");
+            Console.WriteLine($"              |");
+            Console.WriteLine($"              |");
+            Console.WriteLine($"          =========");
+        }
     }
 }
