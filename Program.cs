9ed64e3a7dcd88c7bd3689e11ef84d9c57a56f51
OwenTerhorst <owtea4d@student.oaisd.org>
2025-12-03 15:22:34 +0000

    now added the if statments that say if the counter equals 0 then doesnt add and if its 1 then it adds a head and so on

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        theHangman(true);
+        theHangman(false);
     }
     public static void theHangman(bool n)
     {
@@ -22,7 +22,7 @@ public class Program
             Console.WriteLine($"              |");
             Console.WriteLine($"          =========");
         }
-        else if(counter == 1)
+        else if (counter == 1)
         {
             Console.WriteLine($"          +---+");
             Console.WriteLine($"          |   |");
@@ -31,6 +31,111 @@ public class Program
             Console.WriteLine($"              |");
             Console.WriteLine($"              |");
             Console.WriteLine($"          =========");
+        }
+        else if (counter == 2)
+        {
+            Console.WriteLine($"          +---+");
+            Console.WriteLine($"          |   |");
+            Console.WriteLine($"          0   |");
+            Console.WriteLine($"          |   |");
+            Console.WriteLine($"              |");
+            Console.WriteLine($"              |");
+            Console.WriteLine($"          =========");
+        }
+        else if (counter == 3)
+        {
+            Console.WriteLine(@"          +---+");
+            Console.WriteLine(@"          |   |");
+            Console.WriteLine(@"          0   |");
+            Console.WriteLine(@"         /|   |");
+            Console.WriteLine(@"              |");
+            Console.WriteLine(@"              |");
+            Console.WriteLine(@"          =========");
+        }
+        else if (counter == 4)
+        {
+            Console.WriteLine(@"          +---+");
+            Console.WriteLine(@"          |   |");
+            Console.WriteLine(@"          0   |");
+            Console.WriteLine(@"         /|\  |");
+            Console.WriteLine(@"              |");
+            Console.WriteLine(@"              |");
+            Console.WriteLine(@"          =========");
+        }
+        else if (counter == 5)
+        {
+            Console.WriteLine(@"          +---+");
+            Console.WriteLine(@"          |   |");
+            Console.WriteLine(@"          0   |");
+            Console.WriteLine(@"         /|\  |");
+            Console.WriteLine(@"         /    |");
+            Console.WriteLine(@"              |");
+            Console.WriteLine(@"          =========");
+        }
+        else if (counter == 6)
+        {
+            Console.WriteLine(@"          +---+");
+            Console.WriteLine(@"          |   |");
+            Console.WriteLine(@"          0   |");
+            Console.WriteLine(@"         /|\  |");
+            Console.WriteLine(@"         / \  |");
+            Console.WriteLine(@"              |");
+            Console.WriteLine(@"          =========");
+
+            Thread.Sleep(1500);
+            Console.Clear();
+            Console.WriteLine(".");
+            Thread.Sleep(300);
+            Console.WriteLine(".");
+            Thread.Sleep(300);
+            Console.WriteLine(".");
+            Thread.Sleep(300);
+            Console.WriteLine();
+            Console.WriteLine($"Game Over. Nice Try.");
+            Console.WriteLine();
+            Thread.Sleep(1250);
+            Console.Clear();
+            Console.WriteLine("Want to watch the credits? yes or no");
+            string credits = Console.ReadLine().ToLower();
+            if (credits == "yes")
+            {
+                Console.WriteLine("Credits:");
+                Thread.Sleep(1500);
+                Console.WriteLine();
+                Console.WriteLine("Tyler");
+                Thread.Sleep(1100);
+                Console.WriteLine("Monster feind");
+                Thread.Sleep(900);
+                Console.WriteLine("The Sleeper");
+                Thread.Sleep(900);
+                Console.WriteLine("Light Work he says");
+                Thread.Sleep(1500);
+                Console.WriteLine();
+                Console.Clear();
+                Console.WriteLine("Isaak");
+                Thread.Sleep(1100);
+                Console.WriteLine("Brain behind it all");
+                Thread.Sleep(900);
+                Console.WriteLine("True Gamer");
+                Thread.Sleep(900);
+                Console.WriteLine("Intensive Coder");
+                Thread.Sleep(1500);
+                Console.WriteLine();
+                Console.Clear();
+                Console.WriteLine("Owen");
+                Thread.Sleep(1100);
+                Console.WriteLine("brb");
+                Thread.Sleep(900);
+                Console.WriteLine("ngl");
+                Thread.Sleep(900);
+                Console.WriteLine("tbh");
+                Thread.Sleep(900);
+                Console.Clear();
+            }
+            if(credits == "no")
+            {
+                Console.Clear();
+            }
         }
     }
 }
